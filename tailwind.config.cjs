035bbf9b7f2aa47ffd842c483511f82ffd5ae535
Semiglas <congressmand@gmail.com>
2023-07-17 16:14:38 +0300

    bububu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -45,7 +45,32 @@ module.exports = {
           950: "#351a1a",
         },
       },
+      
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 100 },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.2s ease-in-out forwards",
+      },
     },
+    
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
 };
+
+// ...
+	// extend: {
+    //   keyframes: {
+    //     fadeIn: {
+    //       "0%": { opacity: 0 },
+    //       "100%": { opacity: 100 },
+    //     },
+    //   },
+    //   animation: {
+    //     fadeIn: "fadeIn 0.2s ease-in-out forwards",
+    //   },
+    // },
+    // ...
