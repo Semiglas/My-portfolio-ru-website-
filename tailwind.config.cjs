9e85ee77c6caa94a3045d89c481bebfdf274769c
Semiglas <congressmand@gmail.com>
2023-08-09 14:28:37 +0300

    full ready websites, prior to advance animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -58,7 +58,7 @@ module.exports = {
     },
     
   },
-  plugins: [require("tw-elements/dist/plugin.cjs")],
+  plugins: [require("tw-elements/dist/plugin.cjs"), require('tailwindcss-animated') ],
 };
 
 // ...
